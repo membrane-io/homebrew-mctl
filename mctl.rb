aecf717cc538ed2df91f3e810eaa9aa130c15dae
Juan Campa <juancampa@gmail.com>
2023-09-08 01:35:55 -0400

    Fix x86 typo

--- mctl.rb
+++ mctl.rb
@@ -4,7 +4,7 @@ class Mctl < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/membrane-io/mctl-releases/releases/download/1.6.0/mctl-1.6.0-x86-arm.zip"
+      url "https://github.com/membrane-io/mctl-releases/releases/download/1.6.0/mctl-1.6.0-macos-x86.zip"
       sha256 "c99a79bb015f61222d282f762a5bf99fa6b3ef3fac072555ee66c3f51abdf54f"
     elsif Hardware::CPU.arm?
       url "https://github.com/membrane-io/mctl-releases/releases/download/1.6.0/mctl-1.6.0-macos-arm.zip"
